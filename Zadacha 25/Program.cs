208124164b786d4bc78284aa6e1c99fdffb861fa
Elena <fortygina.lena@mail.ru>
2022-10-31 01:42:21 +0500

    Zadacha 27

--- Zadacha 25/Program.cs
+++ Zadacha 25/Program.cs
@@ -1,37 +1,34 @@
 ﻿/*Задача 25: Напишите цикл, который принимает на вход
- два числа (A и B) и возводит число A в натуральную 
+ два числа (A и B) и возводит число A в натуральную
  степень B.
 3, 5 -> 243 (3⁵)
 2, 4 -> 16*/
 
 int GetTheFirstNumber(string message1)
 {
-Console.WriteLine(message1);
-int result1 = int.Parse(Console.ReadLine());
-return result1;
-
+    Console.WriteLine(message1);
+    int result1 = int.Parse(Console.ReadLine());
+    return result1;
 }
 int GetTheSecondNumber(string message2)
 {
-Console.WriteLine(message2);
-int result2 = int.Parse(Console.ReadLine());
-return result2;
-
+    Console.WriteLine(message2);
+    int result2 = int.Parse(Console.ReadLine());
+    return result2;
 }
 
 int GetExponentiation(int a, int b)
 {
-int expon = 1;
-for (int i = 1; i <= b; i++)
-{
-    expon = expon * a;
-}
-return expon;
-
+    int expon = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        expon = expon * a;
+    }
+    return expon;
 }
 int a = GetTheFirstNumber("Введите число A:");
 int b = GetTheSecondNumber("Введите натуральное число B:");
 
-int expon = GetExponentiation(a,b);
+int expon = GetExponentiation(a, b);
 
 Console.WriteLine($"{a} ^ {b} = {expon}");
---
+++ Zadacha 27/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход
+ число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int GetSummNumber(int number)
+{
+    int summ = 0;
+    int ost = 0;
+    while (number > 0)
+    {
+        ost = number % 10;
+        summ = summ + ost;
+        number = number / 10;
+    }
+    return summ;
+}
+int number = GetNumber("Введите число:");
+int summ = GetSummNumber(number);
+
+Console.WriteLine($"Сумма цифр в числе {number} = {summ}");
